5cdbad4bba8aa4b63bcfdda9977520681196c8eb
Jonatas Marques <jonatasmarquessousa@gmail.com>
2025-08-13 13:10:42 -0300

    faet: tests jest

--- src/users/user.service.spec.ts
+++ src/users/user.service.spec.ts
@@ -45,5 +45,38 @@ describe("UsersService", () => {
         expect(mockPrisma.user.findMany).toHaveBeenCalled();
     })
 
-   
+   it("deve encontrar um usuário pelo id", async () => {
+        const user = { id: '550e8400-e29b-41d4-a716-446655440000', name: "Jonatas", email: "Jonats@gmail.com", password: '123'};
+        mockPrisma.user.findUnique.mockResolvedValue(user);
+
+        const result = await service.findOne('550e8400-e29b-41d4-a716-446655440000');
+        expect(result).toEqual(user);
+        expect(mockPrisma.user.findUnique).toHaveBeenCalledWith({
+            where: { id: '550e8400-e29b-41d4-a716-446655440000' },
+        });
+    })
+
+     it("deve atualizar um usuário", async () => {
+        const dto = { name: "Jonatas", email: "jonatas@gmail.com", password: '123', };
+        const updatedUser = { ...dto, id: '550e8400-e29b-41d4-a716-446655440000' };
+        mockPrisma.user.update.mockResolvedValue(updatedUser);
+
+        const result = await service.update('550e8400-e29b-41d4-a716-446655440000', dto);
+        expect(result).toEqual(updatedUser);
+        expect(mockPrisma.user.update).toHaveBeenCalledWith({
+            where: { id: '550e8400-e29b-41d4-a716-446655440000' },
+            data: dto,
+        }); 
+    })
+
+    it("deve remover um usuário", async () => {
+    const user = { id: '550e8400-e29b-41d4-a716-446655440000', name: "Jonatas", email: "jonatas@gmail.com", password: '123' };
+    mockPrisma.user.delete.mockResolvedValue(user); 
+
+    const result = await service.remove('550e8400-e29b-41d4-a716-446655440000');
+    expect(result).toEqual(user);
+    expect(mockPrisma.user.delete).toHaveBeenCalledWith({
+        where: { id: '550e8400-e29b-41d4-a716-446655440000' },
+    });
+});
 })
