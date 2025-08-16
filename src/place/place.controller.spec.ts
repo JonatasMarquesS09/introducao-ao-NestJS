64bdda471d33b3adb653fe680ad073a7c0b831e0
Jonatas Marques <jonatasmarquessousa@gmail.com>
2025-08-16 11:28:39 -0300

    feat: spec controller place complete

--- src/place/place.controller.spec.ts
+++ src/place/place.controller.spec.ts
@@ -3,6 +3,7 @@ import { PlaceController } from "./place.controller";
 import { PlaceService } from "./place.service";
 import { CloudinaryService } from "./cloudinary.service";
 import { Place, placeType } from "@prisma/client";
+import { Express } from "express";
 
 describe("PlaceController testes", () => {
     let controller: PlaceController;
@@ -15,12 +16,12 @@ describe("PlaceController testes", () => {
             findPaginated: jest.fn(),
             create: jest.fn(),
             update: jest.fn(),
-            remove: jest.fn()
+            delete: jest.fn()
         } as any
 
         const mockCloudinaryService = {
-            uploadImagen: jest.fn(),
-            deleteImagen: jest.fn()
+            uploadImage: jest.fn(),
+            deleteImage: jest.fn()
         } as any
 
         const module: TestingModule = await Test.createTestingModule({
@@ -106,5 +107,121 @@ describe("PlaceController testes", () => {
         expect(placeService.findPaginated).toHaveBeenCalledWith(page, limit);
 
     })
+
+    it("deve criar um novo local", async () => {
+        const place = {
+                name: "Baron",
+                type: placeType.RESTAURANTE,
+                phone: "(88) 4002-8922",
+                latitude: -12344,
+                longitude: 12345,
+                created_at: new Date("2023-01-01")
+        }
+        const files = {
+            images: [
+                { buffer: Buffer.from('https://www.google.com/search?q=restaurante&sxsrf=AE3TifO_jqNVuCCUg_45pfERRQy7lzSujw%3A1755200754088') },
+                { buffer: Buffer.from('https://www.google.com/search?q=restaurante&sxsrf=AE3TifO_jqNVuCCUg_45pfERRQy7lzSujw%3A1755200754088') }
+            ] as Express.Multer.File[]
+        }
+        const images = [
+            'https://www.google.com/search?q=restaurante&sxsrf=AE3TifO_jqNVuCCUg_45pfERRQy7lzSujw%3A1755200754088',
+            'https://www.google.com/search?q=restaurante&sxsrf=AE3TifO_jqNVuCCUg_45pfERRQy7lzSujw%3A1755200754088'
+        ]
+
+        cloudinaryService.uploadImage.mockResolvedValueOnce({ url: images[0], public_id: 'image1' });
+        cloudinaryService.uploadImage.mockResolvedValueOnce({ url: images[1], public_id: 'image2' });
+
+        placeService.create.mockResolvedValue({
+            id: '550e8400-e29b-41d4-a716-446655440000',
+            ...place,
+            images: [
+                { public_id: 'image1', url: images[0] },
+                { public_id: 'image2', url: images[1] },
+            ],
+        });
+
+        const result = await controller.createPlace(place, files);
+
+        expect(result).toEqual({
+            id: '550e8400-e29b-41d4-a716-446655440000',
+            ...place,
+            images: [
+                { public_id: 'image1', url: images[0] },
+                { public_id: 'image2', url: images[1] },
+            ],
+        });
+
+         expect(placeService.create).toHaveBeenCalledWith({
+            ...place,
+            images: [
+                { public_id: 'image1', url: images[0] },
+                { public_id: 'image2', url: images[1] },
+            ],
+        });
+
+    })
+
+    it("deve atualizar  um local", async () => {
+         const place = {
+            name: 'Baron',
+            type: placeType.RESTAURANTE,
+            phone: '(88) 99999-9999',
+            latitude: -3.7327,
+            longitude: -38.5267,
+        };
+
+         const files = {
+            images: [
+                { buffer: Buffer.from('https://www.google.com/search?q=restaurante&sxsrf=AE3TifO_jqNVuCCUg_45pfERRQy7lzSujw%3A1755200754088') },
+                { buffer: Buffer.from('https://www.google.com/search?q=restaurante&sxsrf=AE3TifO_jqNVuCCUg_45pfERRQy7lzSujw%3A1755200754088') },
+            ] as Express.Multer.File[]
+        }
+
+        const imageUrls = [
+            'https://www.google.com/search?q=restaurante&sxsrf=AE3TifO_jqNVuCCUg_45pfERRQy7lzSujw%3A1755200754088',
+            'https://www.google.com/search?q=restaurante&sxsrf=AE3TifO_jqNVuCCUg_45pfERRQy7lzSujw%3A1755200754088',
+        ];
+
+        cloudinaryService.uploadImage.mockResolvedValueOnce({ url: imageUrls[0], public_id: 'image1' });
+        cloudinaryService.uploadImage.mockResolvedValueOnce({ url: imageUrls[1], public_id: 'image2' });
+
+        placeService.update.mockResolvedValue({
+            id: '550e8400-e29b-41d4-a716-446655440000',
+            ...place,
+            images: [
+                { public_id: 'image1', url: imageUrls[0] },
+                { public_id: 'image2', url: imageUrls[1] },
+            ],
+            created_at: new Date(),
+        });
+
+        const result = await controller.updatePlace('550e8400-e29b-41d4-a716-446655440000', place, files);
+
+        expect(result).toEqual({
+            id: '550e8400-e29b-41d4-a716-446655440000',
+            ...place,
+            images: [
+                { public_id: 'image1', url: imageUrls[0] },
+                { public_id: 'image2', url: imageUrls[1] },
+            ],
+            created_at: expect.any(Date),
+        });
+
+        expect(placeService.update).toHaveBeenCalledWith(
+            '550e8400-e29b-41d4-a716-446655440000',
+            place,
+            [Buffer.from('https://www.google.com/search?q=restaurante&sxsrf=AE3TifO_jqNVuCCUg_45pfERRQy7lzSujw%3A1755200754088'), Buffer.from('https://www.google.com/search?q=restaurante&sxsrf=AE3TifO_jqNVuCCUg_45pfERRQy7lzSujw%3A1755200754088')]
+        );
+    })
+
+     it("deve remover um local", async () => {
+        const placeId = '550e8400-e29b-41d4-a716-446655440000';
+
+        placeService.delete.mockResolvedValue(undefined);
+
+        await controller.deletePlace(placeId);
+
+        expect(placeService.delete).toHaveBeenCalledWith(placeId);
+    });
 
 })
